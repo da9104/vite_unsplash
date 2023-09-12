9c9c15249947c37325fcea213bf09e07d8cff280
DAMI <damikang@DAMIs-MacBook-Pro.local>
2023-09-12 14:16:30 +0100

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ dotenv.config(); // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "/vite_unsplash",
   define: {
     __VALUE__: `"${process.env.API}"` // wrapping in "" since it's a string
   },
